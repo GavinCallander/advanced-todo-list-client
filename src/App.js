5e0cc548ed4107fce1a956573c3b3575ba0bb5f4
Gavin Callander <gavincallander@MacBook-Pro.local>
2021-05-27 09:58:32 -0400

    added some more architecture going to run with Sass for this project so I can complete quickly, can use css modules in Next.js project ToDo: get pages built out and work out how I can make listitems customizable for users

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import { Route } from 'react-router-dom';
 
 import { Dashboard, Home, List } from './components/pages';
 
-import './App.css';
+import { Header } from './components/layout';
+
+import './styles/main.scss';
 
 import * as ROUTES from './constants/routes';
 
 export default function App() {
     return (
-        <div className="container">
+        <div className="app">
+            <Header />
             <Route exact path={ROUTES.HOME} render={() =>
                 <Home />
             } />
+            <Route path={ROUTES.DASHBOARD} render={() => {
+                <Dashboard />
+            }} />
+            <Route path={ROUTES.LIST} render={() => {
+                <List />
+            }} />
         </div>
     )
 };
---
+++ src/components/layout/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <div className="header">
+            <span className="header__section">
+                <h1>ToDo</h1>
+            </span>
+            <span className="header__section">
+                <h1>=</h1>
+            </span>
+        </div>
+    )
+};
