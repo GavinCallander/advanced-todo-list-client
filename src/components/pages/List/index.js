4b05a70cc26b9370f07055b5369627fdbb88ac9f
Gavin Callander <gavincallander@MacBook-Pro.local>
2021-06-02 13:41:03 -0400

    data now shows for individual list

--- src/components/pages/List/index.js
+++ src/components/pages/List/index.js
@@ -5,34 +5,34 @@ import axios from 'axios';
 export default function List({ match }) {
 
     const [listId, setListId] = useState(null);
+    const [listData, setListData] = useState({});
 
     useEffect(() => {
-        setListId(match.params.id)
+        setListId(match.params.id);
     }, []);
 
     const fetchListData = () => {
-        console.log(listId);
+        if (!listId) return;
         axios.get(`${process.env.REACT_APP_SERVER_URL}/lists/${listId}`)
         .then(response => {
             console.log(response);
+            setListData(response.data.list);
         })
         .catch(err => {
             console.log(err);
         });
     };
 
+    useEffect(() => {
+        fetchListData();
+    }, [listId]);
+
+    let { collaborators, name, owner, sections } = listData;
+
     return (
         <div className="page">
+            <p className="">{name}</p>
             <Link to="/dashboard">Dashboard</Link>
         </div>
     )
 };
-
-// Hardest part so far is modeling data, drawing out a form to see what it will look like
-// Is this an opportunity to use cool UI for drawing in data?
-// Data flow would be something like
-    // Name of list
-    // Sections (with a what is this drop down for those who don't understand)
-    // Labels (with a what is this drop down for those who don't understand)
-    // ListItems
-// The above will be created from the dashboard page
