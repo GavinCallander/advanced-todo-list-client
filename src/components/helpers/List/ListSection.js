ba627d9d8ae5ea9030e8e00443ba55d41ed5418e
Gavin Callander <gavincallander@MacBook-Pro.local>
2021-06-03 10:25:30 -0400

    just a commit to push smaller changes before the slog need to get the sections and items to display; also to be able to create a new item

--- src/components/helpers/List/ListSection.js
+++ src/components/helpers/List/ListSection.js
@@ -1,3 +1,12 @@
+/*
+    ToDo:
+        *   Section should be a wrapper that takes any listItem components as children
+            *   Section header should also include a newList button; shows on open
+            *   This way, new lists can be easily submitted and attached to a given section
+        *   ListItem should display an individual item, as well as have an intuitive UI
+            *   Allows users to edit, delete, mark as done etc.
+*/
+
 export default function ListSection() {
     return (
         <div className="">
